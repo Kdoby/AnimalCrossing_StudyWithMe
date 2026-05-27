20bd129d8e67e034da302043f69f8e721ea777e8
Kdoby <doyun20445@gmail.com>
2026-05-28 03:45:48 +0900

    feat: 파일 서빙 함수 추가

---
+++ server/utils/streamFile.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+export function streamFile(req, res, filePath) {
+  const stat = fs.statSync(filePath);
+  const range = req.headers.range;
+
+  if (range) {
+    const [startStr, endStr] = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(startStr, 10);
+    const end = endStr ? parseInt(endStr, 10) : stat.size - 1;
+    res.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${stat.size}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': end - start + 1,
+      'Content-Type': 'video/mp4',
+    });
+    fs.createReadStream(filePath, { start, end }).pipe(res);
+  } else {
+    res.writeHead(200, { 'Content-Length': stat.size, 'Content-Type': 'video/mp4' });
+    fs.createReadStream(filePath).pipe(res);
+  }
+}
